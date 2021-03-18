cfc32bdbf1184ece8747306357b8090980e8988a
modnoise <54907481+modnoise@users.noreply.github.com>
2021-03-18 12:39:41 +0200

    Update lab2.py

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 
 var = 306
-m = 6
+m = 15
 
 yMax = (30 - var) * 10
 yMin = (20 - var) * 10
@@ -19,9 +19,9 @@ print(f"Матриця планування при m = {m}:")
 for i in range(3):
     print(Y[i])
 
-Y_average = []
+averageY = []
 for i in range(len(Y)):
-    Y_average.append(np.mean(Y[i]))
+    averageY.append(np.mean(Y[i]))
 
 dispersions = []
 for i in range(len(Y)):
@@ -47,49 +47,49 @@ fuv = [max(dispersions[0], dispersions[1]) / min(dispersions[0], dispersions[1])
 teta = [((m - 2) / m) * fuv[0], ((m - 2) / m) * fuv[1], ((m - 2) / m) * fuv[2]]
 
 ruv = [abs(teta[0] - 1) / sigma_teta, abs(teta[1] - 1) / sigma_teta, abs(teta[2] - 1) / sigma_teta]
-print("Експериментальні значення критерію Романовського:")
-for i in range(3):
-    print(ruv[i])
 
 r_kr = 2
-for i in range(len(ruv)):
-    if ruv[i] > r_kr:
-        print("Неоднорідна дисперсія")
 
+if ruv[0] > r_kr or ruv[1] > r_kr or ruv[2] > r_kr:
+    print("Неоднорідна дисперсія")
+else:
+    print("Експериментальні значення критерію Романовського:")
+    for i in range(3):
+        print(ruv[i])
 
-mx1 = (xn[0][0] + xn[1][0] + xn[2][0]) / 3
-mx2 = (xn[0][1] + xn[1][1] + xn[2][1]) / 3
-my = (Y_average[0] + Y_average[1] + Y_average[2]) / 3
+    mx1 = (xn[0][0] + xn[1][0] + xn[2][0]) / 3
+    mx2 = (xn[0][1] + xn[1][1] + xn[2][1]) / 3
+    my = (averageY[0] + averageY[1] + averageY[2]) / 3
 
-a1 = (xn[0][0] ** 2 + xn[1][0] ** 2 + xn[2][0] ** 2) / 3
-a2 = (xn[0][0] * xn[0][1] + xn[1][0] * xn[1][1] + xn[2][0] * xn[2][1]) / 3
-a3 = (xn[0][1] ** 2 + xn[1][1] ** 2 + xn[2][1] ** 2) / 3
-a11 = (xn[0][0] * Y_average[0] + xn[1][0] * Y_average[1] + xn[2][0] * Y_average[2]) / 3
-a22 = (xn[0][1] * Y_average[0] + xn[1][1] * Y_average[1] + xn[2][1] * Y_average[2]) / 3
+    a1 = (xn[0][0] ** 2 + xn[1][0] ** 2 + xn[2][0] ** 2) / 3
+    a2 = (xn[0][0] * xn[0][1] + xn[1][0] * xn[1][1] + xn[2][0] * xn[2][1]) / 3
+    a3 = (xn[0][1] ** 2 + xn[1][1] ** 2 + xn[2][1] ** 2) / 3
+    a11 = (xn[0][0] * averageY[0] + xn[1][0] * averageY[1] + xn[2][0] * averageY[2]) / 3
+    a22 = (xn[0][1] * averageY[0] + xn[1][1] * averageY[1] + xn[2][1] * averageY[2]) / 3
 
-b0 = determinant(my, mx1, mx2, a11, a1, a2, a22, a2, a3) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
-b1 = determinant(1, my, mx2, mx1, a11, a2, mx2, a22, a3) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
-b2 = determinant(1, mx1, my, mx1, a1, a11, mx2, a2, a22) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
+    b0 = determinant(my, mx1, mx2, a11, a1, a2, a22, a2, a3) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
+    b1 = determinant(1, my, mx2, mx1, a11, a2, mx2, a22, a3) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
+    b2 = determinant(1, mx1, my, mx1, a1, a11, mx2, a2, a22) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
 
-# коефіцієнти нормованих рівнянь регресії
-yNorm1 = b0 + b1 * xn[0][0] + b2 * xn[0][1]
-yNorm2 = b0 + b1 * xn[1][0] + b2 * xn[1][1]
-yNorm3 = b0 + b1 * xn[2][0] + b2 * xn[2][1]
+    # коефіцієнти нормованих рівнянь регресії
+    yNorm1 = b0 + b1 * xn[0][0] + b2 * xn[0][1]
+    yNorm2 = b0 + b1 * xn[1][0] + b2 * xn[1][1]
+    yNorm3 = b0 + b1 * xn[2][0] + b2 * xn[2][1]
 
-dx1 = abs(x1Max - x1Min) / 2
-dx2 = abs(x2Max - x2Min) / 2
-x10 = (x1Max + x1Min) / 2
-x20 = (x2Max + x2Min) / 2
+    dx1 = abs(x1Max - x1Min) / 2
+    dx2 = abs(x2Max - x2Min) / 2
+    x10 = (x1Max + x1Min) / 2
+    x20 = (x2Max + x2Min) / 2
 
-a_0 = b0 - (b1 * x10 / dx1) - (b2 * x20 / dx2)
-a_1 = b1 / dx1
-a_2 = b2 / dx2
+    a_0 = b0 - (b1 * x10 / dx1) - (b2 * x20 / dx2)
+    a_1 = b1 / dx1
+    a_2 = b2 / dx2
 
-# натуралізація рівняння регресії
-yNat1 = a_0 + a_1 * x1Min + a_2 * x2Min
-yNat2 = a_0 + a_1 * x1Max + a_2 * x2Min
-yNat3 = a_0 + a_1 * x1Min + a_2 * x2Max
+    # натуралізація рівняння регресії
+    yNat1 = a_0 + a_1 * x1Min + a_2 * x2Min
+    yNat2 = a_0 + a_1 * x1Max + a_2 * x2Min
+    yNat3 = a_0 + a_1 * x1Min + a_2 * x2Max
 
-print("Середні значення:", Y_average[0], Y_average[1], Y_average[2])
-print("Нормовані коефіцієнти:", round(yNorm1, 4), round(yNorm2, 4), round(yNorm3, 4))
-print("Натуралізовані коефіцієнти:", round(yNat1, 4), round(yNat2, 4), round(yNat3, 4))
+    print("Середні значення:", averageY[0], averageY[1], averageY[2])
+    print("Нормовані коефіцієнти:", round(yNorm1, 4), round(yNorm2, 4), round(yNorm3, 4))
+    print("Натуралізовані коефіцієнти:", round(yNat1, 4), round(yNat2, 4), round(yNat3, 4))
