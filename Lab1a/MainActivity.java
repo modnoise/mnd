1ceb8990ba1abd0cb427e382c2b13ffabe2f1e16
Artem Dromashko <54907481+modnoise@users.noreply.github.com>
2021-05-31 00:27:27 +0300

    Update MainActivity.java

--- Lab1a/MainActivity.java
+++ Lab1a/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,20 +19,25 @@ public class MainActivity extends AppCompatActivity {
         EditText edNumber = findViewById(R.id.textInputEditText);
         TextView text = findViewById(R.id.textView);
         try {
+            long time1 = System.currentTimeMillis();
             int number = Integer.parseInt(edNumber.getText().toString());
             if (number % 2 == 0) {
-                text.setText((number / 2 + " * " + "2"));
+                text.setText((number / 2 + "*" + "2"));
             } else {
                 int x = (int) Math.ceil((Math.sqrt(number)));
                 while (!(Math.pow((int) Math.sqrt(x * x - number), 2) == x * x - number)) {
                     x += 1;
+                    if (System.currentTimeMillis() - time1 > 3000){
+                        text.setText("Перевищений час факторизації");
+                        throw new Exception("error");
+                    }
                 }
                 int y = (int) Math.sqrt(x * x - number);
-                text.setText((x - y) + " * " + (x + y));
+                text.setText((x - y) + "*" + (x + y));
             }
         }
         catch (Exception e){
-            text.setText("Сталася помилка!");
+            text.setText("Час факторизації перевищив 3 секунди");
         }
     }
 }
