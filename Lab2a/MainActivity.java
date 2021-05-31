a5c80463117358490cc2370454fa11d546d0c437
Artem Dromashko <54907481+modnoise@users.noreply.github.com>
2021-05-31 23:43:29 +0300

    Update MainActivity.java

--- Lab2a/MainActivity.java
+++ Lab2a/MainActivity.java
@@ -1,20 +1,12 @@
 package com.example.lab2a;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.TextView;
-import android.view.View;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.textfield.TextInputEditText;
+import static java.lang.Math.random;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,66 +18,66 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @SuppressLint("SetTextI18n")
-    public void Start(View view){
-        long start = System.currentTimeMillis();
-
-        TextInputEditText data0 = findViewById(R.id.editText1);
-        TextInputEditText data1 = findViewById(R.id.editText2);
-        TextInputEditText data2 = findViewById(R.id.editText3);
-
+    public void Start(View view) {
         TextView res = findViewById(R.id.textView6);
+        int count_answers = 0;
+        for (int i = 0; i < 1000; i++) {
 
-        double[] data = {0.01, 0.1, 1000};
+            long start = System.currentTimeMillis();
 
-        data[0] = Double.parseDouble(data0.getText().toString());
-        data[1] = Double.parseDouble(data1.getText().toString());
-        data[2] = Double.parseDouble(data2.getText().toString());
+            double[] data = {0.01, 5, 1000};
+            data[0] = random()*0.3;
+            data[2] = random()*1000;
 
-        double stage = 4;
-        double W1 = 0;
-        double W2 = 0;
+            double stage = 4;
+            double W1 = 0;
+            double W2 = 0;
 
-        double[][] points = { {0.0, 6.0}, {1.0, 5.0}, {3.0, 3.0}, {2.0, 4.0} };
+            double[][] points = {{0.0, 6.0}, {1.0, 5.0}, {3.0, 3.0}, {2.0, 4.0}};
 
-        int count = 0;
-        int succ = 0;
-        int curP = -1;
-        int hod = 0;
-        while (count <= data[2]) {
-            if (succ >= 4) {
-                break;
+            int count = 0;
+            int succ = 0;
+            int curP = -1;
+            int hod = 0;
+            while (count <= data[2]) {
+                if (succ >= 4) {
+                    break;
+                }
+
+                curP = hod % 4;
+                hod++;
+
+                double test = W1 * points[curP][0] + W2 * points[curP][1];
+
+                if (((curP < 2) && test >= stage) || ((curP >= 2) && test < stage)) {
+                    succ++;
+                    continue;
+                } else {
+                    succ = 0;
+                }
+
+                double delta = stage - test;
+
+                W1 += delta * points[curP][0] * data[0];
+                W2 += delta * points[curP][1] * data[0];
+
+                W1 = Math.ceil(W1 * Math.pow(10, 7)) / Math.pow(10, 7);
+                W2 = Math.ceil(W2 * Math.pow(10, 7)) / Math.pow(10, 7);
+
+                if (System.currentTimeMillis() - start >= data[1]) {
+                    break;
+                }
+
+                count++;
             }
 
-            curP = hod%4;
-            hod++;
-
-            double test = W1 * points[curP][0] + W2 * points[curP][1];
-
-            if (((curP < 2) && test >= stage) || ((curP >= 2) && test < stage)) {
-                succ++;
-                continue;
-            } else {
-                succ = 0;
+            if(Double.isNaN(W1)){
+                System.out.println(" ");
             }
-
-            double delta = stage - test;
-
-            W1 += delta * points[curP][0] * data[0];
-            W2 += delta * points[curP][1] * data[0];
-
-            W1 = Math.ceil(W1 * Math.pow(10, 7)) / Math.pow(10, 7);
-            W2 = Math.ceil(W2 * Math.pow(10, 7)) / Math.pow(10, 7);
-
-            if (System.currentTimeMillis() - start >= data[1]) {
-                break;
+            else{
+                count_answers = count_answers + 1;
             }
-
-            count++;
         }
-
-        res.setText("Значення параметрів:\n" +
-                "W1 = " + W1 + "\nW2 = " + W2 +"\nЧас виконання: " +
-                (System.currentTimeMillis() - start) + " мс");
+        res.setText("Корені були знайдені: " + count_answers + " раз");
     }
 }
